640ecebff2cbfdeced8dd5ee0a0e9602a9baa06b
Akash2003webdev <hariaksh418@gmail.com>
2025-11-17 19:18:13 +0530

    img path--

--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -1,22 +1,20 @@
-import { Link } from "react-router-dom";
-import { Card, CardContent } from "@/components/ui";
+import { Link } from 'react-router-dom';
 
+import { Card, CardContent } from '@/components/ui';
 const ListingCard = ({ listing }) => {
   return (
     <Link to={`/listings/${listing.id}`}>
-      <Card className="w-[320px] overflow-hidden rounded-xl shadow">
+      <Card className='w-[320px]'>
         <img
-          className="h-[200px] w-full object-cover"
-          src={`/public/imges/${listing.images?.[0]}`}
-          alt={listing.name || "Listing image"}
+          className='h-[200px] w-full rounded-md object-cover'
+          src={`/src/assets/${listing.images[0]}`}
+          alt={listing.name}
         />
-
-        <CardContent className="p-4">
-          <h2 className="text-xl font-semibold">{listing.name}</h2>
+        <CardContent className='p-4'>
+          <h2 className='mb-0 text-xl font-semibold'>{listing.name}</h2>
         </CardContent>
       </Card>
     </Link>
   );
 };
-
 export default ListingCard;
